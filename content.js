e6e55aa7b746784845dd6d689459cf1c5e960b6f
Hank Henry Hester <hankh@live.unc.edu>
2018-01-05 17:53:49 -0500

    works on all top level domains and sanitizes filenames

--- content.js
+++ content.js
@@ -31,8 +31,12 @@ function turnOff() {
 function handleClick(event) {
   if (event.target.className === 'audio') {
       var link = document.createElement('a');
-      link.href = 'http://www.linguee.com/mp3/' + event.target.id
-      link.download = document.getElementById(event.target.id).parentNode.firstChild.innerHTML // the word beside the audio icon
+      var linkBeginning = /https?:\/\/(www)?.linguee.[^\/]*/g.exec(window.location)[0]
+      console.log(linkBeginning)
+      console.log(event.target.id)
+      link.href = linkBeginning + '/mp3/' + event.target.id
+      link.download = sanitizeFilename(event.target.previousElementSibling.innerText) // the word beside the audio icon
+      console.log(link.href)
 
       //Dispatching click event.
       if (document.createEvent) {
@@ -46,7 +50,7 @@ function handleClick(event) {
 
 function displaySticker() {
   sticker = document.createElement('div')
-  sticker.innerHTML = 'click 🔊 to download'
+  sticker.innerHTML = 'click any 🔊  to download'
   sticker.style.position = 'fixed'
   sticker.style.top = '5px'
   sticker.style.right = '5px'
@@ -62,3 +66,64 @@ function displaySticker() {
 function removeSticker() {
   document.body.removeChild(sticker)
 }
+
+// from: https://github.com/parshap/node-sanitize-filename
+
+/*jshint node:true*/
+'use strict';
+
+/**
+ * Replaces characters in strings that are illegal/unsafe for filenames.
+ * Unsafe characters are either removed or replaced by a substitute set
+ * in the optional `options` object.
+ *
+ * Illegal Characters on Various Operating Systems
+ * / ? < > \ : * | "
+ * https://kb.acronis.com/content/39790
+ *
+ * Unicode Control codes
+ * C0 0x00-0x1f & C1 (0x80-0x9f)
+ * http://en.wikipedia.org/wiki/C0_and_C1_control_codes
+ *
+ * Reserved filenames on Unix-based systems (".", "..")
+ * Reserved filenames in Windows ("CON", "PRN", "AUX", "NUL", "COM1",
+ * "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+ * "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", and
+ * "LPT9") case-insesitively and with or without filename extensions.
+ *
+ * Capped at 255 characters in length.
+ * http://unix.stackexchange.com/questions/32795/what-is-the-maximum-allowed-filename-and-folder-size-with-ecryptfs
+ *
+ * @param  {String} input   Original filename
+ * @param  {Object} options {replacement: String}
+ * @return {String}         Sanitized filename
+ */
+
+function truncate(s, len) {
+  return s.substr(0, len)
+}
+
+var illegalRe = /[\/\?<>\\:\*\|":]/g;
+var controlRe = /[\x00-\x1f\x80-\x9f]/g;
+var reservedRe = /^\.+$/;
+var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
+var windowsTrailingRe = /[\. ]+$/;
+
+function sanitize(input, replacement) {
+  var sanitized = input
+    .replace(illegalRe, replacement)
+    .replace(controlRe, replacement)
+    .replace(reservedRe, replacement)
+    .replace(windowsReservedRe, replacement)
+    .replace(windowsTrailingRe, replacement);
+  return truncate(sanitized, 255);
+}
+
+function sanitizeFilename(input, options) {
+  var replacement = (options && options.replacement) || '';
+  var output = sanitize(input, replacement);
+  if (replacement === '') {
+    return output;
+  }
+  return sanitize(output, '');
+};
